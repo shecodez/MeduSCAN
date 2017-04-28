2f83ed4c73db569f44e74556da1872facfd41e34
shecodez - Nicole J. Nobles <shecodez@gmail.com>
2017-04-28 11:24:34 -0400

    grunt build

--- config/env/development.js
+++ config/env/development.js
@@ -5,7 +5,7 @@ var defaultEnvConfig = require('./default');
 module.exports = {
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
-    //uri: 'mongodb://admin:pass@ds017584.mlab.com:17584/meduscan' || process.env.MONGODB_URI,
+    // uri: 'mongodb://admin:pass@ds017584.mlab.com:17584/meduscan' || process.env.MONGODB_URI,
     options: {
       user: '',
       pass: ''
